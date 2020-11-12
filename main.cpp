abfb6bfe1cbdaad70888b677137896123839aff4
Mathew Denys <mathew@denys.nz>
2020-11-12 13:01:28 +1300

    .gitignore and #pragma updates for macOS development

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 
+#if defined(__has_warning)
+#if __has_warning("-Wreserved-id-macro")
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdocumentation"
+#endif
+#endif
+
 #include <opencv2/core/mat.hpp>  // for basic OpenCV structures (cv::Mat, Scalar)
 #include <opencv2/imgcodecs.hpp> // for reading and writing
 #include <opencv2/highgui.hpp>   // for displaying an image in a window
 #include <opencv2/videoio.hpp>
+
+#if defined(__has_warning)
+#if __has_warning("-Wdocumentation")
+#pragma GCC diagnostic pop
+#endif
+#endif
 
 int main( int argc, char** argv ) // takes one input argument: the name of the input video file
 { 
