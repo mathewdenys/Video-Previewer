8d3adde5c64f80930148479bdd11d1716fb781f9
Mathew Denys <mathew@denys.nz>
2020-11-12 20:35:17 +1300

    Parse very basic config file into strings
    #includes added
    
    
    Formatting

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
 
 #if defined(__has_warning)
 #if __has_warning("-Wreserved-id-macro")
@@ -17,7 +20,52 @@
 #endif
 #endif
 
+// lineParser() parses a single line of the configuration file
+// Returns a std::pair where the key is the name of the configuration option, and the val is the corresponding value
+// Assumes each line is formatted as `LHS = RHS`
+// For now the spaces are mandatory. Eventually I will handle a lack of spaces, and comment lines
+std::pair<std::string, std::string> lineParser(std::string& strIn)
+{
+    std::string key;
+    std::string val;
+
+    std::stringstream ss{ strIn };
+    ss >> key; // LHS of equals sign
+    ss >> val; // The equals sign (will be overritten)
+    ss >> val; // RHS of equals sign
+    std::pair<std::string, std::string> pair{ key, val };
+    return pair;
+}
+
+class ConfigParser
+{
+private:
+    std::string configFilePath;
+
+public:
+    std::map<std::string, std::string> configPairs;
+    ConfigParser(std::string pathIn) : configFilePath{ pathIn }
+    {
+        std::ifstream file{ configFilePath };
+        if (!file)
+            std::cerr << configFilePath << " could not be opened\n";
+
+        std::map<std::string, std::string> configPairsIn;
+
+        while (file)
+        {
+            std::string strInput;
+            std::getline(file, strInput);
+            if (strInput.length() != 0)     // Ignore blank lines
+                configPairsIn.insert(lineParser(strInput));
+        }
+
+        configPairs = configPairsIn;
+    }
+};
+
 int main( int argc, char** argv ) // takes one input argument: the name of the input video file
 {
+    ConfigParser parser("media/.videopreviewconfig");
     return 0;
 }
