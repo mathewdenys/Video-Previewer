0edaf4b27126ef2d40338f9f4e845f501c71fa3c
Mathew Denys <mathew@denys.nz>
2020-11-13 12:18:27 +1300

    Progress towards config option parsing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 #include <map>
 
 #if defined(__has_warning)
@@ -20,6 +21,76 @@
 #endif
 #endif
 
+using configPair = std::pair<std::string, std::string>;
+
+
+enum class OptionType
+{
+    undefined = -1,
+    boolean, // 0
+    integer, // 1
+    string,  // 2
+};
+
+class ConfigOption
+{
+private:
+    std::string optionName;
+
+public:
+    ConfigOption(const std::string& nameIn) : optionName{ nameIn } {}
+    std::string getName() { return optionName; }
+    virtual void print()
+    {
+        std::cout << getName() << ": Undefined option with no value\n";
+    }
+};
+
+class ConfigOptionBool : public ConfigOption
+{
+private:
+    bool value;
+
+public:
+    ConfigOptionBool(const std::string& nameIn, const bool valIn)
+        : ConfigOption{ nameIn }, value{ valIn } {}
+    bool getValue() { return value; }
+    virtual void print()
+    {
+        std::cout << getName() << ": Boolean option with value " << getValue() << '\n';
+    }
+};
+
+class ConfigOptionInt : public ConfigOption
+{
+private:
+    int value;
+
+public:
+    ConfigOptionInt(const std::string& nameIn, const int valIn)
+        : ConfigOption{ nameIn }, value{ valIn } {}
+    int getValue() { return value; }
+    virtual void print()
+    {
+        std::cout << getName() << ": Integer option with value " << getValue() << '\n';
+    }
+};
+
+class ConfigOptionString : public ConfigOption
+{
+private:
+    std::string value;
+
+public:
+    ConfigOptionString(const std::string& nameIn, const std::string& valIn)
+        : ConfigOption{ nameIn }, value{ valIn } {}
+    std::string getValue() { return value; }
+    void print()
+    {
+        std::cout << getName() << ": String option with value " << getValue() << '\n';
+    }
+};
+
 class ConfigParser
 {
 private:
@@ -30,7 +101,7 @@ private:
     // Returns a std::pair where the key is the name of the configuration option, and the val is the corresponding value
     // Assumes each line is formatted as `LHS = RHS`
     // For now the spaces are mandatory. Eventually I will handle e.g. `LHS=RHS`, and comment lines
-    std::pair<std::string, std::string> lineParser(std::string& strIn)
+    configPair lineParser(std::string& strIn)
     {
         std::string key;
         std::string val;
@@ -39,18 +110,53 @@ private:
         ss >> key; // LHS of equals sign
         ss >> val; // The equals sign (will be overritten)
         ss >> val; // RHS of equals sign
-        std::pair<std::string, std::string> pair{ key, val };
+        configPair pair{ key, val };
         return pair;
+    }
+    
+    // test if the string `testString` corresponods to an integer
+    // uses the std::stringstream extraction operator, which performs casts if it can
+    bool isInt(const std::string& testString)
+    {
+        int myInt;
+        std::stringstream testStringStream{ testString };
+        if(!(testStringStream >> myInt))
+            return false;
+        return true;
+    }
+    
+    bool stringToBool(const std::string& str)
+    {
+        if (str == "true")
+            return true;
+        return false;
+    }
+    
+    int stringToInt(const std::string& str)
+    {
+        int myInt;
+        std::stringstream ss{ str };
+        ss >> myInt;
+        return myInt;
+    }
+    
+    OptionType optionTypeIdentifier(const configPair& pair)
+    {
+        if (pair.second == "true" || pair.second == "false")
+            return OptionType::boolean;
+        else if (isInt(pair.second))
+            return OptionType::integer;
+        return OptionType::string; // Assume to be a string by default (do better checking for validity here)
     }
 
 public:
-    ConfigParser(std::string pathIn) : configFilePath{ pathIn }
+    ConfigParser(const std::string& pathIn) : configFilePath{ pathIn }
     {
         std::ifstream file{ configFilePath };
         if (!file)
             std::cerr << configFilePath << " could not be opened\n";
 
-        std::map<std::string, std::string> configPairsIn;
+        std::map<std::string, std::string> configPairsIn; // why am I declaring a dummy variable only to assign it to configPairs later?
 
         while (file)
         {
@@ -62,6 +168,18 @@ public:
 
         configPairs = configPairsIn;
     }
+    
+    ConfigOption pairToConfigOption(const configPair& pair)
+    {
+        if (optionTypeIdentifier(pair) == OptionType::boolean)
+            return ConfigOptionBool(pair.first, stringToBool(pair.second));
+        else if (optionTypeIdentifier(pair) == OptionType::integer)
+            return ConfigOptionInt(pair.first, stringToInt(pair.second));
+        else
+            return ConfigOptionString(pair.first, pair.second);
+    }
+    
+    std::map<std::string, std::string> getPairs() { return configPairs; }
 
     void print()
     {
@@ -70,9 +188,51 @@ public:
     }
 };
 
+class VideoPreview
+{
+private:
+    cv::VideoCapture          video;
+    std::vector<cv::Mat>      frames;
+    std::vector<ConfigOption> options;
+    
+public:
+    VideoPreview(const std::string& videoPathIn, const std::string& configPathIn)
+    {
+        // import the video file
+        video = cv::VideoCapture(videoPathIn);
+        if (!video.isOpened())
+            std::cout  << "Could not open video: " << videoPathIn << '\n';
+        
+        // load the configuration file and make `options`
+        options.reserve(2); // temporary value for now
+        ConfigParser parser(configPathIn);
+        for (auto& pair : parser.getPairs())
+            options.push_back(parser.pairToConfigOption(pair));
+        
+        for (auto& el : options)
+            el.print();
+        
+        
+        // create the frames
+            
+    }
+};
+
+
 int main( int argc, char** argv ) // takes one input argument: the name of the input video file
 {
-    ConfigParser parser("media/.videopreviewconfig");
-    parser.print();
+    std::string videoPath{ "media/sunrise.mp4" };
+    std::string configPath{ "media/.videopreviewconfig" };
+    
+    VideoPreview vid(videoPath, configPath);
+    
+    /*ConfigOption o1{ "optionOne" };
+    std::cout << o1.getName() << std::endl;
+    ConfigOption o2{ "optionTwo" };
+    std::cout << o2.getName() << std::endl;
+    ConfigOptionBool o3{ "optionBool", true };
+    std::cout << o3.getName() << std::endl;*/
+    
+    
     return 0;
 }
