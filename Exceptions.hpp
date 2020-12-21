c9e64e7c6b846cbc8412fe943511336a74014aed
Mathew Denys <mathew@denys.nz>
2020-12-21 17:08:13 +1300

    Create Exceptions.hpp

---
+++ Exceptions.hpp
@@ -0,0 +1,13 @@
+//
+//  Exceptions.hpp
+//  Video-Previewer
+//
+//  Created by Mathew Denys on 21/12/20.
+//
+
+#ifndef Exceptions_hpp
+#define Exceptions_hpp
+
+#include <stdio.h>
+
+#endif /* Exceptions_hpp */
