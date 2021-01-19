db6aee673fdd497b465890ab0630ce1913eaf7e4
Mathew Denys <mathew@denys.nz>
2021-01-19 18:06:31 +1300

    Make the side panel width adaptive to fill unused space

--- Video-Previewer-GUI/ContentView.swift
+++ Video-Previewer-GUI/ContentView.swift
@@ -7,6 +7,17 @@
 
 import SwiftUI
 
+/*----------------------------------------------------------------------------------------------------
+    MARK: - Constants
+   ----------------------------------------------------------------------------------------------------*/
+
+let almostBlack = Color(red: 0.2, green: 0.2, blue: 0.2, opacity: 1.0); // For text and shapes
+
+let frameWidth        = 200.0 // The width of the individual frames in the video preview
+let previewPadding    = 15.0  // The padding around the frames in the video preview
+let scrollBarWidth    = 15.0  // The width of a scrollbar in a ScrollView
+let sidePanelMinWidth = 300.0 // The miniumum width of the side panel
+
 
 /*----------------------------------------------------------------------------------------------------
     MARK: - ContentView
@@ -14,13 +25,44 @@ import SwiftUI
 
 struct ContentView: View {
     let vp = VideoPreviewWrapper("/Users/mathew/Library/Containers/mdenys.Video-Previewer-GUI/Data/sunrise.mov")
+    var frames: [NSImage?]
     
     var body: some View {
-        HStack(spacing:0) {
-            PreviewPaneView(vp: self.vp!)
-            SidePanelView(vp: self.vp!)
-                .frame(maxWidth: 300)
+        GeometryReader { geometry in
+            // Determine a lower bound for the width of all the on-screen elements *except* the actual frames that are being previewed
+            //  i.e. the width of the side panel + the width of a scrollbar on the side panel and on the preview pane + padding on each side of the preview
+            let minWidthOfNonFrameElements: Double = sidePanelMinWidth + 2.0*scrollBarWidth + 2.0*previewPadding
+            
+            // Determine an upper bound for the width of the frames being previewed
+            let widthOfWindow:     Double = Double(geometry.size.width)
+            let maxWidthOfPreview: Double = widthOfWindow - minWidthOfNonFrameElements
+            
+            // Determine the number of frames per row given the current window size
+            //  i.e. the maximum number of images of width `frameWidth` that can fit into an area with the width of the screen minus the width of all the non-frame elements
+            let cols: Int = Int(maxWidthOfPreview / frameWidth)
+            
+            // Determine the number of rows required to display the frames
+            let rows: Int = (frames.count / cols) + 1
+            
+            // Determine the actual width of the entire video preview pane
+            // The width of the side panel will adjust to fill the remaining space, with a minimum width given by `sidePanelMinWidth`
+            //  i.e. The width of the actual frames displayed + the scrollbar + padding on each side
+            let previewWidth: Double = frameWidth*Double(cols) + scrollBarWidth + 2.0*previewPadding
+            
+            HStack(spacing:0) {
+                PreviewPaneView(frames: self.frames, cols: cols, rows: rows)
+                    .frame(width: CGFloat(previewWidth))
+                SidePanelView(vp: self.vp!)
+            }
         }
+    }
+    
+    init() {
+        vp!.loadConfig()
+        vp!.loadVideo()
+        vp!.updatePreview()
+        
+        frames = vp!.getFrames()
     }
 }
 
--- Video-Previewer-GUI/PreviewPane.swift
+++ Video-Previewer-GUI/PreviewPane.swift
@@ -8,15 +8,6 @@
 import SwiftUI
 
 /*----------------------------------------------------------------------------------------------------
-    MARK: - Constants
-   ----------------------------------------------------------------------------------------------------*/
-
-let frameWidth     = CGFloat(200.0) // The width of each preview frame
-let previewPadding = CGFloat(15.0)  // The padding around the video preview
-let scrollBarWidth = CGFloat(15.0)  // Width of a scroll bar in a ScrollView
-
-
-/*----------------------------------------------------------------------------------------------------
     MARK: - FramePreviewView
    ----------------------------------------------------------------------------------------------------*/
 
@@ -27,37 +18,7 @@ struct FramePreviewView: View {
         Image(nsImage: image)
             .resizable()
             .aspectRatio(contentMode: .fit)
-            .frame(width: frameWidth)
-    }
-}
-
-
-/*----------------------------------------------------------------------------------------------------
-    MARK: - FrameTableView
-   ----------------------------------------------------------------------------------------------------*/
-
-struct FrameTableView: View {
-    let cols: Int
-    let rows: Int
-    var frames:  [NSImage?]
-    
-    var body: some View {
-        ScrollView() {
-            VStack(alignment:.leading){
-                ForEach(0..<rows, id: \.self) { i in
-                    HStack(spacing: 0) {
-                        ForEach(0..<cols, id: \.self) { j in
-                            let index = i*cols + j
-                            if (index < frames.count)
-                            {
-                                FramePreviewView(image: frames[i*cols+j] ?? NSImage())
-                            }
-                        }
-                        Spacer()
-                    }
-                }
-            }.padding([.top, .leading, .bottom], previewPadding)
-        }
+            .frame(width: CGFloat(frameWidth))
     }
 }
 
@@ -67,36 +28,37 @@ struct FrameTableView: View {
    ----------------------------------------------------------------------------------------------------*/
 
 struct PreviewPaneView: View {
-    let vp: VideoPreviewWrapper
     var frames: [NSImage?]
+    let cols: Int
+    let rows: Int
     
     var body: some View {
         ZStack {
             Color(red: 0.98, green: 0.98, blue: 0.98, opacity: 1.0).edgesIgnoringSafeArea(.all)
             VStack {
-                GeometryReader { geometry in
-                    let c = Int(floor((geometry.size.width - scrollBarWidth - previewPadding) / frameWidth))
-                    let r = (frames.count / c) + 1
-                    
-                    FrameTableView(cols: c, rows: r, frames: self.frames)
+                ScrollView() {
+                    VStack(alignment:.leading){
+                        ForEach(0..<rows, id: \.self) { i in
+                            HStack(spacing: 0) {
+                                ForEach(0..<cols, id: \.self) { j in
+                                    let index = i*cols + j
+                                    if (index < frames.count)
+                                    {
+                                        FramePreviewView(image: frames[i*cols+j] ?? NSImage())
+                                    }
+                                }
+                                Spacer()
+                            }
+                        }
+                    }.padding(.all, CGFloat(previewPadding))
                 }
-                Spacer()
             }
         }
     }
-    
-    init(vp: VideoPreviewWrapper) {
-        self.vp = vp
-        self.vp.loadConfig()
-        self.vp.loadVideo()
-        self.vp.updatePreview()
-        
-        frames = vp.getFrames()
-    }
 }
 
-struct PreviewPane_Previews: PreviewProvider {
-    static var previews: some View {
-        PreviewPaneView(vp: VideoPreviewWrapper("/Users/mathew/Library/Containers/mdenys.Video-Previewer-GUI/Data/sunrise.mov"))
-    }
-}
+//struct PreviewPane_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PreviewPaneView()
+//    }
+//}
